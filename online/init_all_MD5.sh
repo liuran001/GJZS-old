daef345dd48c0e8929bb2ced982c150025001e22
liuran001 <liuran001@126.com>
2021-05-15 02:18:12 +0800

    try to fix a bug...

--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -1,6 +1,6 @@
 # a() { md5sum < `pm path $Package_name | sed 's/.*://g'`; }
 # 
-# Canary=2021050802
+# Canary=2021051501
 # if [[ $Version_code -eq $Canary ]]; then
     # if [[ `a` != 779dd22b55d0fbec37462bfe64582b6a* ]]; then
         # echo "！盗版软件，已触发自动卸载"
@@ -14,3 +14,4 @@
     Util_Functions_MD5=ba4cf8e696e0b764695277fc005c343c
     Cloud_ID=Cloud_Page.zip
     Cloud_Version=2021051501
+    Cloud_MD5=1aefd34a0483630004b939bd299c3dca
