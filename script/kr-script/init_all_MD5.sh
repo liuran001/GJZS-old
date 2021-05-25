d3a801add75b5b4179fdb8cc7db127c36d42a3bc
liuran001 <liuran001@126.com>
2021-05-25 20:10:25 +0800

    修复

--- script/kr-script/init_all_MD5.sh
+++ script/kr-script/init_all_MD5.sh
@@ -9,7 +9,7 @@
     # fi
 # fi
     init_data_ID=init_data.sh
-    init_data_MD5=c2df191fd3f3f6271199881a8a232a0f
+    init_data_MD5=74a7b0427cff7b588253853e40bbd4d3
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=a6f88b6780a598dc32858a392bd01d26
     Cloud_ID=Cloud_Page.zip
