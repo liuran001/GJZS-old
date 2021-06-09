a8a54f933b110eaf3e269d3b9fa44a6bff2ea2a5
liuran001 <liuran001@126.com>
2021-06-09 22:53:28 +0800

    update

--- APK_unpacked/Offline/assets/usr/kr-script/init_all_MD5.sh
+++ APK_unpacked/Offline/assets/usr/kr-script/init_all_MD5.sh
@@ -13,5 +13,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=ef518b7ceb042bd7b69fc17dc5e417c8
     Cloud_ID=Cloud_Page.zip
-    Cloud_Version=2021060902
+    Cloud_Version=2021060903
     Cloud_MD5=''
--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -13,5 +13,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=d6d598cc84e4ef662430fcec145f46e5
     Cloud_ID=Cloud_Page.zip
-    Cloud_Version=2021060902
-    Cloud_MD5=6557541e7352e89d63cce349a5ac4ecf
+    Cloud_Version=2021060903
+    Cloud_MD5=8afd83d84db962ae2d34c477b04f168a
