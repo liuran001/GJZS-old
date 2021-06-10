5b3ccf9925590562c3fe942c3a18aa4b1d976ce0
liuran001 <liuran001@126.com>
2021-06-11 00:49:48 +0800

    就挺离谱的（

--- APK_unpacked/Offline/assets/usr/kr-script/init_all_MD5.sh
+++ APK_unpacked/Offline/assets/usr/kr-script/init_all_MD5.sh
@@ -1,5 +1,5 @@
     init_data_ID=init_data.sh
-    init_data_MD5=96dc1842573617fecf2c8a3a0268956d
+    init_data_MD5=4e2a76376a4d9e750c9836ffebe15042
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=ef518b7ceb042bd7b69fc17dc5e417c8
     Cloud_ID=Cloud_Page.zip
--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -1,5 +1,5 @@
     init_data_ID=init_data.sh
-    init_data_MD5=68d92b71a6eb92b5e533bc393b579f29
+    init_data_MD5=7d388dac57efbb8bb7d1e2ff63b8da3a
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=d6d598cc84e4ef662430fcec145f46e5
     Cloud_ID=Cloud_Page.zip
