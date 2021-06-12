a8854dfc7064c03c56f5abf86b4e293eeeca0693
liuran001 <liuran001@126.com>
2021-06-13 01:28:58 +0800

    修改页面

--- online/Cloud_Page/kr-script/CheckUpdate.sh
+++ online/Cloud_Page/kr-script/CheckUpdate.sh
@@ -8,9 +8,6 @@ V() {
     echo "功能版本：$Util_Functions_Code"
     echo "配置版本：$Configuration"
     echo "$1版本：$Version_Name（$Version_code）"
-    echo "本重制版包括Q群/TG群组免费提供，自愿打赏
-唯一官方承认发布渠道为酷安@快播内部工作人员 以及官网gjzs.qqcn.xyz（或gjzs.qqcn.site）（包括其附属蓝奏云链接和coding/github仓库）
-如果有人倒卖，请不遗余力在全平台举报并且曝光他！"
 }
 
 if [[ -f $Core ]]; then
--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=720cc960c8af34ca84e0d5fae5b8cd91
     Cloud_ID=Cloud_Page.zip
-    Cloud_Version=2021061301
-    Cloud_MD5=4834df6587beb1a6f95649372548b716
+    Cloud_Version=2021061302
+    Cloud_MD5=45a8fd82ddfbcd26a49ad3d9a287319a
