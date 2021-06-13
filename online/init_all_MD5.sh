777a5c914e3a5fc8fe68b93c7121ab421bffa6c9
liuran001 <liuran001@126.com>
2021-06-13 22:26:17 +0800

    这点破事更新个屁啊（

--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=720cc960c8af34ca84e0d5fae5b8cd91
     Cloud_ID=Cloud_Page.zip
-    Cloud_Version=2021061305
-    Cloud_MD5=e81bc168ccd8428be33e19c6f85b6a43
+    Cloud_Version=2021061306
+    Cloud_MD5=cc47ac5a079cf73f1876fb8f0dcddafd
