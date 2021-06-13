2600f8a2a5914765bfee85b9dc51d493a606714e
liuran001 <liuran001@126.com>
2021-06-14 02:25:56 +0800

    端午安康！

--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=720cc960c8af34ca84e0d5fae5b8cd91
     Cloud_ID=Cloud_Page.zip
-    Cloud_Version=2021061401
-    Cloud_MD5=1b14578dca0509ce706b0c2e939036ce
+    Cloud_Version=2021061402
+    Cloud_MD5=d57a5deb79785e859275a3026b51f9a1
