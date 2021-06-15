70f5432ef606aa130d1436d7fdb66bf18f9eeb01
liuran001 <liuran001@126.com>
2021-06-15 23:26:19 +0800

    同步#2 到云端版本

--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=720cc960c8af34ca84e0d5fae5b8cd91
     Cloud_ID=Cloud_Page.zip
-    Cloud_Version=2021061403
-    Cloud_MD5=edd073656a1cad7aa39a3804ac742522
+    Cloud_Version=2021061501
+    Cloud_MD5=0eafd71815c5a4bd8c9263eda009c725
