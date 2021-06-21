86c5da874aada1d71bdca2ef5ad81473dc125136
liuran001 <liuran001@126.com>
2021-06-21 21:48:17 +0800

    修复自选制作开机动画功能无法使用问题

---
+++ APK_unpacked/Offline/assets/usr/kr-script/BootAnimation/command.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+FILE="/data/adb/bootanimation_make"
+FFMPEG="${TOOLKIT}/ffmpeg"
+FFMPEGMD5="efab65c8885bd5594996d41fd5d13b1b"
+mkdir -p $FILE
+if [[ ! -f $FFMPEG ]]
+then
+	echo "--联网下载ffmpeg，稍等"
+	cd $FILE
+	curl -O https://od.qqcn.site/Other/ffmpeg/ffmpeg.a[a-u]?raw
+	cat ffmpeg.a* > ffmpeg
+	cp -f ffmpeg $FFMPEG
+	cd - > /dev/null
+fi
+
+md5sum $FFMPEG | grep $FFMPEGMD5 > /dev/null
+[[ $? -eq 0 ]] || (echo "ffmpeg下载出现问题，返回重新下载" && rm -f $FFMPEG)
+
+chmod 777 $FFMPEG
+alias ffmpeg=$FFMPEG
