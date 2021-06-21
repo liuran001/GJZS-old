3f5e735f755c9194dbd9029fbfea9bed1c932fad
liuran001 <liuran001@126.com>
2021-06-21 22:37:36 +0800

    修复LSPosed模块介绍显示错误

--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=720cc960c8af34ca84e0d5fae5b8cd91
     Cloud_ID=Cloud_Page.zip
-    Cloud_Version=2021062104
-    Cloud_MD5=99013007d3ded332c3ed5b06bf1ff96b
+    Cloud_Version=2021062105
+    Cloud_MD5=feb5b39ed542a6441ff80f15bd1485a7
