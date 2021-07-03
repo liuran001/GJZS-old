45e2d42244c6841194278605f1cdd7fcc9988896
liuran001 <liuran001@126.com>
2021-07-03 17:11:18 +0800

    改md5

--- online/init_all_MD5.sh
+++ online/init_all_MD5.sh
@@ -1,7 +1,7 @@
     init_data_ID=init_data.sh
-    init_data_MD5=7d2a40f3135f2ebb2d5ee954a716e1bd
+    init_data_MD5=e00b601853ff7a639ee344c3ee58165b
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=720cc960c8af34ca84e0d5fae5b8cd91
     Cloud_ID=Cloud_Page.zip
-    Cloud_Version=2021062902
-    Cloud_MD5=c56a84f8d45079ded65151a0f660564c
+    Cloud_Version=2021070301
+    Cloud_MD5=e55613f5109ddb47402c60112600ccc7
